49e1d42be7d8fb95f8ef093a8133c6e2954cc40a
Srikanth <srikanth.turlapati@kysu.edu>
2021-02-06 15:15:09 -0500

    Modified hello.cpp: Now accepts a name as input and diplays a greting

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <String>
+
 int main() {
-std::cout << "Hello World!" << std::endl;
+	string name;
+	std::cout <<  "Enter Name:";
+	std::cin >> name;
+        std::cout << "Hello " <<name << std::endl;
 return 0;
 } 
